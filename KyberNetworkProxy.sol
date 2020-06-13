c7e15656325b78034f4e55d878e421197fd29dd8
Carlos Rwi <65631167+Carlos-RWI@users.noreply.github.com>
2020-06-14 01:33:51 +0200

    Update KyberNetworkProxy.sol

--- KyberNetworkProxy.sol
+++ KyberNetworkProxy.sol
@@ -1,24 +1,7 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.16 <0.6.0;
 
-interface IERC20 {
-    
-    function totalSupply() external view returns (uint256);
+import { IERC20 as ERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-   
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
 
 interface KyberNetworkProxyInterface {
     function maxGasPrice() external view returns(uint);
